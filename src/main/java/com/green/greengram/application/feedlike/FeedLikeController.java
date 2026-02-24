445c8a0e19874a594846201ed9cd6bb064e6ef4b
younggeun <qwazsx1346@gmail.com>
2026-02-24 11:37:37 +0900

    feed_like 작업중

--- src/main/java/com/green/greengram/application/feedlike/FeedLikeController.java
+++ src/main/java/com/green/greengram/application/feedlike/FeedLikeController.java
@@ -1,7 +1,13 @@
 package com.green.greengram.application.feedlike;
 
+import com.green.greengram.application.feedlike.model.FeedLikeReq;
+import com.green.greengram.configuration.model.ResultResponse;
+import com.green.greengram.configuration.model.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/feed/like")
 public class FeedLikeController {
     private final FeedLikeService feedLikeService;
+
+    @PostMapping
+    public ResultResponse<?> toggleFeedLike(@AuthenticationPrincipal UserPrincipal userPrincipal
+                                            ,@RequestBody FeedLikeReq req) {
+        req.setSignedUserId( userPrincipal.getSignedUserId() );
+        log.info("req: {}", req);
+        boolean result = feedLikeService.toggleFeedLike( req );
+        return new ResultResponse<>(result ? "좋아요 처리" : "좋아요 취소", result);
+    }
 }
