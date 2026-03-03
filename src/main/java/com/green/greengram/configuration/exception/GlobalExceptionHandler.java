7cfc4db0340326e8d3971cf8316bf07236e7d853
younggeun <qwazsx1346@gmail.com>
2026-03-03 17:36:15 +0900

    토큰재발행

--- src/main/java/com/green/greengram/configuration/exception/GlobalExceptionHandler.java
+++ src/main/java/com/green/greengram/configuration/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.green.greengram.configuration.exception;
 
 import com.green.greengram.configuration.model.ResultResponse;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -10,10 +12,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import tools.jackson.databind.json.JsonMapper;
+
+import java.security.SignatureException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +44,29 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
         .body(new ResultResponse<>(sb.toString(), errors.toString()));
+  }
+
+  @ExceptionHandler(ResponseStatusException.class)
+  public ResponseEntity<ResultResponse<String>> handleResponseStatusException(ResponseStatusException ex) {
+    log.error("ResponseStatusException: {}", ex.getReason());
+
+    String statusMessage = HttpStatus.valueOf(ex.getStatusCode().value()).getReasonPhrase();
+
+    return ResponseEntity.status(ex.getStatusCode())
+                          .body(new ResultResponse<>(statusMessage, ex.getReason()));
+  }
+
+  //토큰에 무슨 문제가 발생되었을 때.
+  @ExceptionHandler({MalformedJwtException.class, SignatureException.class})
+  public ResponseEntity<Object> handleMalformedJwtException() {
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        .body(new ResultResponse<>("토큰을 확인해 주세요.",null));
+  }
+
+  @ExceptionHandler(ExpiredJwtException.class) //토큰이 만료가 되었을 때
+  public ResponseEntity<Object> handleExpiredJwtException() {
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        .body(new ResultResponse<>("토큰을 확인해 주세요.",null));
   }
 
   private List<ValidationError> getValidationError(BindException e) {
